f23d239e84edc16463982ede474255baad04715c
RonaLu <rona26lu@gmail.com>
2021-01-06 23:20:57 +0800

    add search queue

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
 require 'econfig'
+require 'roda'
+require 'delegate'
+require 'sequel'
 
-module CodePraise
+module Ewa
   # Setup config environment
-  class CloneReportWorker
+  class SearchWorker < Roda
+    plugin :environments
+
     extend Econfig::Shortcut
     Econfig.env = ENV['WORKER_ENV'] || 'development'
     Econfig.root = File.expand_path('..', File.dirname(__FILE__))
+
+    configure :development, :test do
+      ENV['DATABASE_URL'] = "sqlite://#{config.DB_FILENAME}"
+    end
+
+    configure :production do
+      # Set DATABASE_URL environment variable on production platform
+      use Rack::Cache,
+        verbose: true,
+        metastore: config.REDISCLOUD_URL + '/0/metastore',
+        entitystore: config.REDISCLOUD_URL + '/0/entitystore'
+    end
+
+    configure do
+      require 'sequel'
+      DB = Sequel.connect(ENV['DATABASE_URL']) # rubocop:disable Lint/ConstantDefinitionInBlock
+
+      def self.DB # rubocop:disable Naming/MethodName
+        DB
+      end
+    end
   end
 end
